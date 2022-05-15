4aa4a748420d2e3f95d725321d5d42984f0122f1
Nahuel Agustin Alberti <105518230+Jibade17@users.noreply.github.com>
2022-05-14 21:27:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int numeroSecreto = new
+Random(DateTime.Now.Millisecond).Next(1, 21);
+
+
+var contador = 0;
+int numero = 0;
+
+do
+{
+    contador++;
+
+    Console.WriteLine("Ingrese un número");
+    numero = int.Parse(Console.ReadLine());
+    
+    if (numero == numeroSecreto)
+    {
+        Console.WriteLine("Felicitaciones, has adivinado el número que era: " + numeroSecreto);
+        Console.WriteLine("Lo has logrado en " + contador + " intentos.");
+    }
+    else if (numero > numeroSecreto)
+    { 
+        Console.WriteLine("El numero es menor");
+    }
+    else
+    {
+        Console.WriteLine("El numero es mayor");
+    }
+}
+while ( numero != numeroSecreto);
+
+
